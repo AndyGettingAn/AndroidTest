7abe2dff40a30695f30635002bca41592a856fec
Andreas Filipiuk <andreas-filipiuk@web.de>
2018-10-26 11:59:48 +0200

    initil

---
+++ app/src/main/java/de/ur/mi/myapplication/MainActivity.kt
@@ -0,0 +1,17 @@
+package de.ur.mi.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //New Code
+
+        //Second Code
+        //Third Code
+    }
+}
